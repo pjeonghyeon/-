7db844635301fa693affc2b7371d5bb1a8767484
pjeonghyeon <133744289+pjeonghyeon@users.noreply.github.com>
2023-10-25 16:32:39 +0900

    10/18일 실습
    Object에 대한 정리

---
+++ 자바 7주차.js
@@ -0,0 +1,36 @@
+function printHello() {
+    console.log('Hello');
+}
+printHello();
+
+function log(message) {
+    console.log(message);
+}
+log('Hello@');
+log(1234);
+
+function changeName(obj) {
+    obj.name='conder';
+}
+const ellie={name: 'ellie'};
+changeName(ellie);
+console.log(ellie);
+
+// Local scope
+
+let globalMessage='global';
+function printMessage() {
+    let message='hello';
+    console.log(message);
+    console.log(globalMessage);
+    function printAnother() {
+        console.log(message);
+        let childMessage='hello';
+    }
+    console.log(childMessage);
+}
+printMessage();
+
+
+
+
